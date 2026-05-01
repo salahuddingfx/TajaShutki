5f8177c4bf727dd4de9a25c3a32cdced00349d5b
salahuddingfx <salauddinkaderappy@gmail.com>
2026-05-02 01:01:52 +0600

    feat: initialize React entry point with Redux, Helmet, and Toaster providers

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import App from '@/App.jsx'
 import { Provider } from 'react-redux'
 import { store } from '@/store'
 import { HelmetProvider } from 'react-helmet-async'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <HelmetProvider>
         <App />
+        <Toaster position="top-right" />
       </HelmetProvider>
     </Provider>
   </StrictMode>,
