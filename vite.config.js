300581929fa0e27f727bc41ffbeeedde94d77950
salahuddingfx <salauddinkaderappy@gmail.com>
2026-05-08 11:13:37 +0600

    feat: configure path alias for src directory in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -27,7 +28,13 @@ export default defineConfig({
       }
     })
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     port: 5174,
   },
 })
+
